646027d905a94440e3f4c5249bd7ac667df06617
Lavkush Nishad <lovekushnishad003@gmail.com>
2026-05-30 21:53:09 +0530

    React Component

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,22 +1,15 @@
 import "./App.css";
 import Title from "./Title.jsx";
+import ProductTab from "./ProductTab.jsx";
 
-function Title(){
-  return <h1>I am the title</h1>
-}
-
-function Description(){
-  return <h3>I am the Description!</h3>
-}
 function App() {
   
 
   return( 
-  <div>
-  <Title/>
-  <Description/>
-
-  </div>
+  <>
+  <ProductTab/>
+</>
+  
   );
 }
 
--- React/src/Title.jsx
+++ React/src/Title.jsx
@@ -1,5 +1,15 @@
 function Title(){
-    return <h1>I am the Title</h1>
+
+let name="lavkush";
+    return ( 
+    <div>
+      <h1>I am the Title</h1>
+      <h3>I am the Description</h3>
+      <p>2*2={2*2}</p>
+      <p>Hi, {name.toUpperCase()}</p>
+    </div>
+
+    );
 }
 
 //epxport
